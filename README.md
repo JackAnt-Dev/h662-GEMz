fd3e04fb574c14296c2aa8272814fe4e6d620be0
JackAnt-Dev <jackant.dev@gmail.com>
2022-03-25 22:33:26 +0900

    3강 완료

--- README.md
+++ README.md
@@ -6,4 +6,8 @@ project 실습 from h662GEMz
 
 next 사용이유
 
--   import fs 가 Module not found 에러 띄우는 경우 (서버 사이드에서 돌아가야하는데 클라에서 돌아가서 발생하는 에러) react에서는 웹팩 건드려야해서 어려운데 next에선 next.config.js 만 건드리면 되서 편함.
+- import fs 가 Module not found 에러 띄우는 경우 (서버 사이드에서 돌아가야하는데 클라에서 돌아가서 발생하는 에러) react에서는 웹팩 건드려야해서 어려운데 next에선 next.config.js 만 건드리면 되서 편함.
+
+배포시 uri 입력할때 주의사항
+
+- json 파일의 ipfs 주소를 입력해야함. image 말고
--- contracts/MintGem.sol
+++ contracts/MintGem.sol
@@ -3,8 +3,10 @@
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol"; // 확장된 ERC721
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract MintGem is ERC721Enumerable {
+contract MintGem is ERC721Enumerable, Ownable {
     string uri;
     constructor(string memory _name, string memory _symbol, string memory _uri) ERC721(_name, _symbol) {
         uri = _uri;
@@ -17,6 +19,9 @@ contract MintGem is ERC721Enumerable {
 
     mapping(uint => GemData) public gemData;
 
+    // 1 klay
+    uint gemPrice = 100000000000000000; // 10^18
+
     function tokenURI(uint _tokenId) override public view returns (string memory) {  // 이미 ERC721에 존재하는 함수. Opensea에서 인식하게 만드려면 이 이름으로 꼭 정의해야한다고.
         string memory gemRank = Strings.toString(gemData[_tokenId].gemRank);
         string memory gemType = Strings.toString(gemData[_tokenId].gemType);
@@ -26,8 +31,12 @@ contract MintGem is ERC721Enumerable {
         );
     }
 
-    function mintGem() public {
+    function mintGem() public payable {
+        require(gemPrice <= msg.value, "Not enough Klay."); // minting 은 가격 지불해야만 가능
+
         uint tokenId = totalSupply() + 1;
+
+        payable(owner()).transfer(msg.value);    // owner(): Owner에서 사용 가능, 이 컨트랙을 발행한 지갑의 주소를 출력
 
         gemData[tokenId] = GemData(4, 4);
 
